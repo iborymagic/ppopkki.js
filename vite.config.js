14f79130bb5655b142054e7fec0c928f394f95ab
agrajak <korbots@gmail.com>
2022-09-28 12:48:52 +0900

    chore: add base url for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 /** @type {import('vite').UserConfig} */
 export default {
     root: 'src',
+    base: 'https://iborymagic.github.io/ppopkki.js',
     build: {
         assetsDir: 'public',
         outDir: '..',
