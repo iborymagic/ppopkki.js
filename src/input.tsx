dbb1caeec4991e2a6514c43160b6ff88c3c703bf
agrajak <korbots@gmail.com>
2022-11-01 20:14:18 +0900

    :lipstick: 카드 색깔 고르는 select 추가 및 N도 저장 - this closes #20, #16

--- src/input.tsx
+++ src/input.tsx
@@ -2,13 +2,32 @@ import React, { useEffect, useState } from "react";
 import { nanoid } from "nanoid";
 import "bulma";
 
+// https://github.com/ymssx/ygo-card/tree/master/source/mold/frame
+const cardTypes = [
+  "monster",
+  "monster_cl",
+  "monster_lj",
+  "monster_rh",
+  "monster_tc",
+  "monster_tk",
+  "monster_tt",
+  "monster_xg",
+  "monster_ys",
+  "spell",
+  "trap",
+] as const;
+
+type CardType = typeof cardTypes[number];
+
 type Item = {
   checked: boolean;
   id: string;
   name: string;
   url: string;
+  type: CardType;
 };
 const STORAGE_KEY = "table";
+const N_KEY = "n";
 
 declare global {
   interface Window {
@@ -28,6 +47,7 @@ const defaultItem: Item = {
   name: "",
   url: "",
   checked: true,
+  type: "monster",
 };
 
 const isItems = (value: unknown): value is Item[] => {
@@ -49,6 +69,7 @@ const isItems = (value: unknown): value is Item[] => {
 
 function Input() {
   const [items, setItems] = useState<Item[]>([defaultItem]);
+  const [n, setN] = useState<number | undefined>(undefined);
 
   const removeState = (id: string) => {
     setItems((items) => items.filter((item) => item.id !== id));
@@ -68,11 +89,15 @@ function Input() {
   const getIdForName = (id: string) => id + "name";
   const getIdForURL = (id: string) => id + "url";
   const getIdForCheck = (id: string) => id + "checked";
+  const getIdForCardType = (id: string) => id + "type";
 
   useEffect(() => {
     try {
       const arr = JSON.parse(localStorage.getItem(STORAGE_KEY) ?? "");
+      const nFromStorage = parseInt(localStorage.getItem(N_KEY) ?? "");
+      console.log({ nFromStorage });
 
+      setN(isNaN(nFromStorage) ? 1 : nFromStorage);
       if (isItems(arr)) {
         setItems(arr);
       }
@@ -98,6 +123,7 @@ function Input() {
               <th> </th>
               <th>이름(required)</th>
               <th>이미지 주소(optional)</th>
+              <th>카드 종류(required)</th>
               <th> </th>
             </thead>
             <tbody>
@@ -127,6 +153,20 @@ function Input() {
                       ></input>
                     </td>
                     <td>
+                      <div className="select">
+                        <select
+                          id={getIdForCardType(item.id)}
+                          defaultValue={item.type}
+                        >
+                          {cardTypes.map((typeName) => (
+                            <option key={typeName} value={typeName}>
+                              {typeName}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    </td>
+                    <td>
                       {items.length > 1 && (
                         <button
                           className="button is-danger is-small"
@@ -140,7 +180,7 @@ function Input() {
                 );
               })}
               <tr>
-                <td rowSpan={4}>
+                <td colSpan={5}>
                   <button className="button is-info" onClick={addState}>
                     한 명 더 추가하기
                   </button>
@@ -148,13 +188,18 @@ function Input() {
               </tr>
             </tbody>
           </table>
-          <p>
+          <p className="mb-3" style={{ display: "flex", alignItems: "center" }}>
+            <b>총 {items.length}명</b>
             중에서
             <input
               id="n"
-              className="input"
+              style={{
+                maxWidth: 50,
+                margin: "0 5px",
+              }}
+              className="input is-small"
               type="number"
-              defaultValue={1}
+              defaultValue={n}
             ></input>
             명을 뽑을 거에요.
           </p>
@@ -162,7 +207,7 @@ function Input() {
             [경고] 이미지 주소칸을 비우면, 귀여운 고양이 사진이 들어갈 수도
             있습니다.
           </div>
-          <p>
+          <p style={{ display: "flex", justifyContent: "flex-end" }}>
             <button
               className="button is-primary"
               type="submit"
@@ -171,35 +216,55 @@ function Input() {
                 const form = document.forms[0];
 
                 const result = items.map((item) => {
-                  const name = form[getIdForName(item.id)]?.value ?? "";
-                  const url = form[getIdForURL(item.id)]?.value ?? "";
+                  const name =
+                    form[getIdForName(item.id)]?.value ?? defaultItem.name;
+                  const url =
+                    form[getIdForURL(item.id)]?.value ?? defaultItem.url;
+                  const cardType =
+                    form[getIdForCardType(item.id)]?.value ?? defaultItem.type;
                   const checked =
-                    form[getIdForCheck(item.id)]?.checked ?? false;
+                    form[getIdForCheck(item.id)]?.checked ??
+                    defaultItem.checked;
+
+                  const typeObj = {
+                    type: cardType.split("_")[0],
+                    type2:
+                      cardType.split("_").length > 1
+                        ? cardType.split("_")[1]
+                        : undefined,
+                  };
+
                   return {
                     data: {
                       name,
+                      ...typeObj,
                     },
                     pic: url,
                     checked,
+                    type: cardType,
                   };
                 });
 
                 const itemsForStorage: Item[] = result.map((item, idx) => {
                   return {
                     id: nanoid(),
-                    name: item.data.name ?? "",
-                    url: item.pic ?? "",
-                    checked: item.checked ?? false,
+                    name: item.data.name ?? defaultItem.name,
+                    url: item.pic ?? defaultItem.url,
+                    checked: item.checked ?? defaultItem.checked,
+                    type: item.type ?? defaultItem.type,
                   };
                 });
+
+                const n = form["n"]?.value ?? 1;
 
                 window.localStorage.setItem(
                   STORAGE_KEY,
                   JSON.stringify(itemsForStorage)
                 );
+                window.localStorage.setItem(N_KEY, String(n));
                 window.onSubmit(
                   result.filter((x) => x.checked),
-                  form["n"]?.value ?? 1
+                  n
                 );
               }}
             >
